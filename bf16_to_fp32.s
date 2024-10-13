66d8b4d963337630c241ca8d1965326e9af63e2c
yenshipotato <55551372+yenshipotato@users.noreply.github.com>
2024-10-13 21:50:52 +0800

    Create bf16_to_fp32.s

---
+++ bf16_to_fp32.s
@@ -0,0 +1,9 @@
+.data
+        argument: .word   0x00003e80 #0.25(bf16)
+  
+.text
+bf16_to_fp32:     
+
+        lw t1, argument  # Load the argument into register t1
+        li t2, 16        # Load offset (16) into register t2
+        sll t3, t1, t2   # Shift left the contents in register t1 by 16 bits and places the result into register t3.
