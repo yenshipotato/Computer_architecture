0ef98fb92936e586188799c348f267f9c132ce90
yenshipotato <55551372+yenshipotato@users.noreply.github.com>
2024-10-14 14:22:45 +0800

    Add return value
    Move return value to a0

--- bf16_to_fp32.s
+++ bf16_to_fp32.s
@@ -7,3 +7,4 @@ bf16_to_fp32:
         lw t1, argument  # Load the argument into register t1
         li t2, 16        # Load offset (16) into register t2
         sll t3, t1, t2   # Shift left the contents in register t1 by 16 bits and places the result into register t3.
+        mv a0, t3 
